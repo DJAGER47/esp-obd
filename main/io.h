b115b8b4d6b4346dfca74c631a3a8270ffd3a87e
Pavel Lavrovskiy <plavrovskiy@yandex-team.ru>
2025-07-09 11:51:02 +0300

    2

---
+++ main/io.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// 0	GPIO0	ADC1
+// 1	GPIO1	ADC1
+// 2	GPIO2	ADC1, boot mode / strapping pin
+// 3	GPIO3	ADC1
+// 4	GPIO4	ADC1, JTAG
+// 5	GPIO5	JTAG
+// 6	GPIO6	JTAG
+// 7	GPIO7	JTAG
+// 8	GPIO8	Blue status_led (inverted), boot mode / strapping pin
+// 9	GPIO9	Boot mode / strapping pin, boot button
+// 10	GPIO10
+// 20	GPIO20
+// 21	GPIO21
+
+#define LED_GPIO CONFIG_BLINK_GPIO  ///< LED 8
