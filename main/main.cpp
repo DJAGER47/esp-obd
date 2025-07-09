6fd61ee9a9611654b8315a132714932b25f41206
Pavel Lavrovskiy <plavrovskiy@yandex-team.ru>
2025-07-09 17:20:26 +0300

    2

--- main/main.cpp
+++ main/main.cpp
@@ -2,30 +2,13 @@
 
 #include "CANDriver.hpp"
 #include "PIDParser.hpp"
-#include "driver/uart.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-void initUART() {
-  uart_config_t uart_config = {.baud_rate = 115200,
-                               .data_bits = UART_DATA_8_BITS,
-                               .parity    = UART_PARITY_DISABLE,
-                               .stop_bits = UART_STOP_BITS_1,
-                               .flow_ctrl = UART_HW_FLOWCTRL_DISABLE};
-  uart_param_config(UART_NUM_0, &uart_config);
-  uart_set_pin(UART_NUM_0,
-               UART_PIN_NO_CHANGE,
-               UART_PIN_NO_CHANGE,
-               UART_PIN_NO_CHANGE,
-               UART_PIN_NO_CHANGE);
-  uart_driver_install(UART_NUM_0, 1024, 0, 0, NULL, 0);
-}
-
 extern "C" void app_main() {
   ESP_LOGI("APP", "Starting application");
 
-  initUART();
   CANDriver driver;
   PIDParser parser;
 
